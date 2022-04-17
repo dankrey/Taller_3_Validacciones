9655a39a8dc0be65d59d4595fdaff72e98956d7c
dankrey <dereyes@uniboyaca.edu.co>
2022-04-17 15:27:28 -0500

    modal notifiacion

--- JQUERY/jquery.js
+++ JQUERY/jquery.js
@@ -155,7 +155,15 @@ function FindId(id){
 };
 
 
+
+
 function deleteTema() {
+
+ 
+
+  
+
+
 
   var msg = "¡Tema actualizado con éxito!";
   $("#userTable > tbody").html("");
@@ -173,6 +181,9 @@ function deleteTema() {
     //i es para  eleminar elementos primer parametro donde va eliminar y el segundo es la cantidad de elementos ah eleminar
 
       temas.splice(index,1)
+
+     
+      
   }
 
   temas.forEach(function(user,i) {
@@ -219,17 +230,31 @@ function Messaje_info(msg) {
 }
 
 function agregarTemaTabla(user) {
-  $("#userTable > tbody:last-child").append(`
-        <tr id="user-${user.id}">
-            <td class="datosObjt" name="name">${user.name} <a class="Card_VerMas" href="Formulario_Agregar.html">Ver Más....</a></td>
-            '<td  class="datosObjt" name="Codigo">${user.Categoria}</td>
-            '<td class="datosObjt" type="date" name="Fecha_Reg">${user.Fecha_Reg}</td>
-             <td  class="datosObjt" type="date" name="Fecha_Mod">${user.Fecha_Mod}</td>
-           
-            </td>
-        </tr>
+  $("#userTable >").append(`
+
+  
+  <tr id="user-${user.id}">
+  <td class="datosObjt" name="name">${user.name} <a class="Card_VerMas" href="Formulario_Agregar.html">Ver Más....</a></td>
+  
+        
     `);
 }
+
+
+       
+
+
+
+
+
+{/* <tr id="user-${user.id}">
+<td class="datosObjt" name="name">${user.name} <a class="Card_VerMas" href="Formulario_Agregar.html">Ver Más....</a></td>
+'<td  class="datosObjt" name="Codigo">${user.Categoria}</td>
+'<td class="datosObjt" type="date" name="Fecha_Reg">${user.Fecha_Reg}</td>
+ <td  class="datosObjt" type="date" name="Fecha_Mod">${user.Fecha_Mod}</td>
+
+</td>
+</tr> */}
 
 
 
@@ -285,6 +310,111 @@ function Fecha_Validar() {
   
   }
 };
+
+
+
+
+
+
+
+
+
+
+
+function jmgModal(id, data, ok, cancel, input) {
+	data=data || {};
+	id="modal-"+id;
+	if (document.getElementById(id)==null) {
+		var d=document.createElement("div");
+		d.className="jmgmodal";
+		d.id=id;
+		var p=document.createElement("div");
+		p.className="panel";
+		var t=document.createElement("div");
+		t.className="title";
+		var cl=document.createElement("div");
+		cl.className="close";
+		cl.innerHTML='&times;';
+		cl.addEventListener('click',function(ev) {
+			ev.preventDefault();
+			var dTop=this.parentNode.parentNode;
+			dTop.classList.remove("visible");
+			dTop.querySelector(".panel .content").innerHTML='';
+		});
+		var ct=document.createElement("div");
+		ct.className="content";
+		var f=document.createElement("div");
+		f.className="footer";
+		p.appendChild(t);p.appendChild(cl);p.appendChild(ct);p.appendChild(f);
+		d.appendChild(p);
+		document.body.appendChild(d);
+	}
+	var mod=document.getElementById(id),
+	p=mod.querySelector(".panel"),
+	t=mod.querySelector(".panel .title"),
+	ct=mod.querySelector(".panel .content"),
+	f=mod.querySelector(".panel .footer");
+	if (f==null) {
+		mod.classList.remove("nofooter");
+		var f=document.createElement("div");
+		f.className="footer";
+		p.appendChild(f);
+	}
+	t.innerHTML=data.title || '';
+	ct.innerHTML=data.content || '';
+	f.innerHTML='';
+	if (!isNaN(data.width)) p.style.maxWidth=data.width+'px';
+	if (!isNaN(data.height)) p.style.maxHeight=data.height+'vh';
+	if (ok && ok.length>1) {
+		var param={value:null};
+		if (input && input.length>0) {
+			var ph=document.createElement("p");
+			ph.className="action";
+			var txt=document.createElement("input");
+			txt.className="action";
+			txt.setAttribute("placeholder",input[0]);
+			txt.addEventListener('keydown',function(ev) {
+				if (ev.keyCode==13 || ev.key=="Enter") {
+					ev.preventDefault();
+					mod.classList.remove("visible");
+					ok[1](param.value);
+				}
+			});
+			ph.appendChild(txt); ct.appendChild(ph);
+			param=ct.querySelector("p.action > input.action");
+			setTimeout(function(){
+				param.focus();
+			},100);
+		}
+		var bOk=document.createElement("button");
+		bOk.className="action";
+		bOk.innerHTML=ok[0];
+		bOk.addEventListener('click',function(ev) {
+			ev.preventDefault();
+			mod.classList.remove("visible");
+			ok[1](param.value);
+		});
+		f.appendChild(bOk);
+	}
+	if (cancel && cancel.length>1) {
+		var bCancel=document.createElement("button");
+		bCancel.className="action";
+		bCancel.innerHTML=cancel[0];
+		bCancel.addEventListener('click',function(ev) {
+			ev.preventDefault();
+			mod.classList.remove("visible");
+			cancel[1]();
+		});
+		f.appendChild(bCancel);
+	}
+	if (f.innerHTML=='') {
+		p.removeChild(f);
+		mod.classList.add("nofooter");
+	}
+	setTimeout(function(){
+		mod.classList.add("visible");
+	},50);
+}
 
 
 
